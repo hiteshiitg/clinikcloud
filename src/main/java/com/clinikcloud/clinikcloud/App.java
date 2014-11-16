a6708ae4ed671b65789ad5c603b0620e5910f480
hiteshiitg <hiteshiitg@gmail.com>
2014-11-16 22:25:40 +0530

    test

---
+++ src/main/java/com/clinikcloud/clinikcloud/App.java
@@ -0,0 +1,13 @@
+package com.clinikcloud.clinikcloud;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
